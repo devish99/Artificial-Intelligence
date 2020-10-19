bbc834485e06e2f458d43661a787caee07a00c37
Vishnu <54411695+devish99@users.noreply.github.com>
2020-10-20 00:21:44 +0530

    Add files via upload

---
+++ medical_report.pl
@@ -0,0 +1,13 @@
+relieves(crocin, headache).
+relieves(gelusil, nausea).
+relieves(wikoril, cough).
+relieves(eno, acidity).
+
+aggravates(salted_chips, high_blood_pressure).
+aggravates(butter_chicken, diabetes).
+
+should_take(Person, Drug) :- complains_of(Person, Symptom), relieves(Drug, Symptom), not(unsuitable_for(Person, Drug)).
+unsuitable_for(Food, Person) :- aggravates(Food, Condition), suffers_from(Person, Condition).
+
+complains_of(vishnu, acidity).
+suffers_from(vishnu, diabetes).
